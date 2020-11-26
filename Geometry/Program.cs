6a4e26f028af981bb5e494a803702bbcadf86b30
Julien.Iz <69205008+IzyJu@users.noreply.github.com>
2020-11-26 12:07:19 +0100

    Add files via upload

---
+++ Geometry/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Geometry
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Polygone carrer = new Square { cote = 10 };
+            Polygone rect = new Rectangle { height = 10, width = 8 };
+            Console.WriteLine(rect.area + " est le perimetre du rectangle et " + carrer.area + " du carrer.");
+
+        }
+    }
+    public abstract class Polygone
+    {
+        public virtual double area { get; }
+
+    }
+    class Rectangle : Polygone
+    {
+        public double height { get; set; }
+        public double width { get; set; }
+        public override double area
+        {
+            get{ return height * 2 + width * 2; }
+        }
+        public void SetWidth(double width)
+        {
+            this.width = width;
+        }
+
+        public void SetHeight(double height)
+        {
+            this.height = height;
+        }
+    }
+
+    class Square : Polygone
+    {
+        public double cote { get; set; }
+        public override double area
+        {
+            get { return cote * 4; }
+        }
+        public void SetWidth(double width)
+        {
+            cote = width;
+        }
+        public void SetHeight(double height)
+        {
+            cote = height;
+        }
+    }
+}
